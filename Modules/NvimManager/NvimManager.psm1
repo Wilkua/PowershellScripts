7c5fdda159be320bfd72e19727585a8a1934323b
William Drescher <wilkua2@gmail.com>
2021-07-22 01:40:11 -0400

    add first pass powershell module for nvim

---
+++ Modules/NvimManager/NvimManager.psm1
@@ -0,0 +1,120 @@
+function Test-NvimManagerRequirements
+{
+	[CmdletBinding()]
+	param(
+		[switch] $Report
+	)
+
+	$success = $true
+	if ($Report)
+	{
+		Write-Host ''
+		Write-Host 'Neovim Manager Module Requirements'
+		Write-Host '----------------------------------'
+	}
+
+	$gitInstalled = Get-Command -ErrorAction SilentlyContinue "git"
+	if (-Not $gitInstalled)
+	{
+		$success = $false
+		if ($Report)
+		{
+			Write-Host -NoNewline 'Git ................ '
+			Write-Host -ForegroundColor Red 'Not Installed'
+		}
+	}
+	else
+	{
+		if ($Report)
+		{
+			Write-Host -NoNewline 'Git .................... '
+			Write-Host -ForegroundCOlor Green 'Installed'
+		}
+	}
+
+	return $success
+}
+
+function _ensurePluginPath
+{
+	param(
+		[Parameter(Mandatory, Position=0)]
+		[string] $Path
+	)
+
+	if (-Not (Test-Path -Path $Path))
+	{
+		$_ = New-Item -ItemType 'Directory' -Path $nvimPluginPath
+	}
+
+	if (-Not (Test-Path -Path $Path))
+	{
+		return $false
+	}
+
+	return $true
+}
+
+function Get-NvimPluginPath
+{
+	# Note: This should probalby invoke nvim and have it echo stdpath('data')
+	[CmdletBinding()]
+	param (
+		[switch] $EnsurePath
+	)
+
+	$userLocalAppData = "$Env:USERPROFILE\AppData\Local"
+	$nvimPluginPath = "$userLocalAppData\nvim-data\site\pack\0\start"
+
+	if ($EnsurePath)
+	{
+		if (-Not (_ensurePluginPath $nvimPluginPath))
+		{
+			return ''
+		}
+	}
+
+	return $nvimPluginPath
+}
+
+function Install-NvimPlugin
+{
+	[CmdletBinding()]
+	param(
+		[Parameter(ValueFromPipeline, Position=0)]
+		[string] $GithubRepo,
+		[string] $GitUrl
+	)
+
+	process
+	{
+		if (-Not (Test-NvimManagerRequirements))
+		{
+			Write-Error -Message 'Neovim Manager requirements are not met - Run Check-NvimManagerRequirements -Report for more information'
+			return false
+		}
+
+		$nvimPluginPath = Get-NvimPluginPath -EnsurePath
+		if (-Not $nvimPluginPath)
+		{
+			Write-Error -Message "Failed to create plugin install path $nvimPluginPath"
+			return ''
+		}
+	}
+}
+
+function Get-NvimInstalledPlugins
+{
+	[CmdletBinding()]
+	param (
+		[switch] $WithPath
+	)
+
+	$nvimPluginPath = Get-NvimPluginPath -EnsurePath
+	$plugins = Get-ChildItem -Path $nvimPluginPath
+
+	return $plugins
+}
+
+Export-ModuleMember -Function 'Get-NvimInstalledPlugins', 'Install-NvimPlugin', 'Get-NvimPluginPath', 'Test-NvimManagerRequirements'
+
